9e242218131f0b2fd5bb1358827d2493ca0384e0
AAGAN <AAGAN@users.noreply.github.com>
2017-06-23 13:51:31 -0400

    Create FIA.cpp

---
+++ FIA.cpp
@@ -0,0 +1,15 @@
+#include "FIA.h"
+
+int fia_method
+(
+  double& A, //total vent area in (m^2)
+  double M, //mass flow of the extinguishing agent (kg/s)
+  double S, //specific vapor volume of extinguishant (cubic meters/kg)
+  double P, //maximum room strength (Pa)
+  double C, //design concentration (%)
+  double T //temperature (K)
+)
+{
+
+return 0;
+}
