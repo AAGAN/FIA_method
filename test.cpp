cd3ac9aeb5574e38bc2be6e1ee7d7e3ac8f3edd3
Arash Agan <AAGAN@users.noreply.github.com>
2017-07-11 07:52:08 -0400

    just for the presentation

--- test.cpp
+++ test.cpp
@@ -6,7 +6,23 @@ int main()
 {
     
 double A = 1.0;
+/**
+test case for non liqifiable agents
+*/
+int error_code =  fia_method
+(
+  A, //total vent area in (m^2)
+  1.0, //mass flow of the extinguishing agent (kg/s)
+  1.0, //specific vapor volume of extinguishant (cubic meters/kg)
+  1000.0, //maximum room strength (Pa)
+  46.0, //design concentration (%)
+  370.0 //temperature (K)
+);
 
+    
+/**
+test cases for liqifiable agents
+*/
 int error_code =  fia_method
 (
   A, //total vent area in (m^2)
